3562536efd0ae1e835a75750a8708effc3be0dfc
Siddarth Chatla <siddarth_chatla@Siddarths-MacBook-Pro.local>
2021-11-21 22:17:15 +0530

    fisrt commit

---
+++ hello.py
@@ -0,0 +1,29 @@
+
+print("hello world");
+
+a = 23;
+b = 33;
+
+print(a,b);	
+
+#immutable strings tuples numbers
+
+c = 22.43
+print(c);
+
+#condition statements 
+
+if(a>b):
+
+	print("a is greater");
+
+elif(b>c):
+
+	print("b is greater");
+
+
+else:
+
+	print("c is greater");
+
+#loops 
